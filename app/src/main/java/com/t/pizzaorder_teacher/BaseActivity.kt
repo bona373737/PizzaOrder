a95751cd861fffae158197c80e4267e6c8bb9be4
bona <bona373737@gmail.com>
2020-11-15 15:19:39 +0900

    Initial commit

---
+++ app/src/main/java/com/t/pizzaorder_teacher/BaseActivity.kt
@@ -0,0 +1,12 @@
+package com.t.pizzaorder_teacher
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity:AppCompatActivity() {
+
+    val mContext = this
+
+    abstract fun setupEvents()
+
+    abstract fun setValues()
+}
