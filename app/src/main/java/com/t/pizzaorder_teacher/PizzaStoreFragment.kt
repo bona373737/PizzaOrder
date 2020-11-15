5129e29f84f638667e71ff26ed56f1fda2a67941
bona <bona373737@gmail.com>
2020-11-15 15:41:56 +0900

    피자가게 프래그먼트 초안 작성중

---
+++ app/src/main/java/com/t/pizzaorder_teacher/PizzaStoreFragment.kt
@@ -0,0 +1,19 @@
+package com.t.pizzaorder_teacher
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentPagerAdapter
+
+class PizzaStoreFragment: Fragment {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_pizza_order,container,false)
+    }
+}
